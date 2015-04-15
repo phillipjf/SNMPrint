376e333e980aa509ec5166b1974c3e5a7a3da617
Phillip Ferentinos <pjf0001@auburn.edu>
2015-04-15 11:33:34 -0500

    Added simple GUI and SNMP class

--- app/src/main/java/edu/auburn/eng/csse/comp3710/pjf0001/snmprint/MainActivity.java
+++ app/src/main/java/edu/auburn/eng/csse/comp3710/pjf0001/snmprint/MainActivity.java
@@ -1,9 +1,12 @@
 package edu.auburn.eng.csse.comp3710.pjf0001.snmprint;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +15,26 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final String READ_COMMUNITY = "public";
+        final int    mSNMPVersion = 0; // 0 represents SNMP version=1
+        final String BASE_IP = "131.204.116.";
+        final String OID_BASE_LEVEL = "1.3.6.1.2.1.43.11.1.1.9.";
+        final String OID_BLACK = "1.1";
+        final String OID_YELLOW = "1.2";
+        final String OID_CYAN = "1.3";
+        final String OID_MAGENTA = "1.4";
+
+
+        final PrintStatus stat = new PrintStatus();
+        Button submit = (Button) findViewById(R.id.submitButton);
+        submit.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                stat.snmpGet(BASE_IP+findViewById(R.id.ipEnd).toString(), READ_COMMUNITY, mSNMPVersion,OID_BASE_LEVEL+OID_BLACK);
+                stat.snmpGet(BASE_IP+findViewById(R.id.ipEnd).toString(), READ_COMMUNITY, mSNMPVersion,OID_BASE_LEVEL+OID_YELLOW);
+                stat.snmpGet(BASE_IP+findViewById(R.id.ipEnd).toString(), READ_COMMUNITY, mSNMPVersion,OID_BASE_LEVEL+OID_CYAN);
+                stat.snmpGet(BASE_IP+findViewById(R.id.ipEnd).toString(), READ_COMMUNITY, mSNMPVersion,OID_BASE_LEVEL+OID_MAGENTA);
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/edu/auburn/eng/csse/comp3710/pjf0001/snmprint/PrintStatus.java
@@ -0,0 +1,71 @@
+package edu.auburn.eng.csse.comp3710.pjf0001.snmprint;
+/*
+ * http://hosteddocs.ittoolbox.com/KA110807.pdf
+ * http://www.oidview.com/mibs/0/Printer-MIB.html
+ */
+
+import android.app.Activity;
+import android.widget.TextView;
+import java.net.InetAddress;
+import snmp.*;
+
+public class PrintStatus extends Activity{
+	public static final String OID_BASE_LEVEL = "1.3.6.1.2.1.43.11.1.1.9.";
+	public static final String OID_BLACK = "1.1";
+	public static final String OID_YELLOW = "1.2";
+	public static final String OID_CYAN = "1.3";
+	public static final String OID_MAGENTA = "1.4";
+
+	/*
+	 * This method process the request and Get the Value on the device
+	 * @returns String. community=READ_COMMUNITY
+	 */
+	public void snmpGet(String strIPAddress, String community, int iSNMPVersion, String strOID){
+		String str="";
+		try{
+			InetAddress hostAddress = InetAddress.getByName(strIPAddress);
+			
+			SNMPv1CommunicationInterface comInterface = new SNMPv1CommunicationInterface(iSNMPVersion, hostAddress, community);
+			
+			// returned Vector has just one pair inside it.
+			SNMPVarBindList newVars = comInterface.getMIBEntry(strOID);
+			
+			// extract the (OID,value) pair from the SNMPVarBindList; the pair is just a two-element SNMPSequence
+			SNMPSequence pair = (SNMPSequence)(newVars.getSNMPObjectAt(0));
+			
+			// extract the object identifier from the pair; it's the first element in the sequence
+			//SNMPObjectIdentifier snmpOID = (SNMPObjectIdentifier)pair.getSNMPObjectAt(0);
+			
+			//extract the corresponding value from the pair; it's the second element in the sequence
+			SNMPObject snmpValue = pair.getSNMPObjectAt(1);
+
+			//printGUI.model.addElement(printGUI.printerName.getText());
+            TextView view = (TextView) findViewById(R.id.outputText);
+			str = snmpValue.toString();
+			if(strOID == OID_BASE_LEVEL+OID_BLACK){
+				//view.addElement("BLACK: " + str + "%");
+                view.append("BLACK" + str + "%");
+				System.out.println("BLACK: "+ str +"%");
+			}
+			else if(strOID == OID_BASE_LEVEL+OID_YELLOW){
+				//printGUI.model.addElement("YELLOW: "+ str +"%");
+                view.append("Yellow" + str + "%");
+				System.out.println("YELLOW: "+ str +"%");
+			}
+			if(strOID == OID_BASE_LEVEL+OID_CYAN){
+				//printGUI.model.addElement("CYAN: "+ str +"%");
+                view.append("CYAN" + str + "%");
+				System.out.println("CYAN: "+ str +"%");
+			}
+			if(strOID == OID_BASE_LEVEL+OID_MAGENTA){
+				//printGUI.model.addElement("MAGENTA: "+ str +"%");
+                view.append("MAGENTA" + str + "%");
+                System.out.println("MAGENTA: "+ str +"%");
+			}
+		}
+		catch(Exception e){
+			System.out.println("Exception during SNMP operation: " + e + "\n");
+		}
+	}
+} 
+
