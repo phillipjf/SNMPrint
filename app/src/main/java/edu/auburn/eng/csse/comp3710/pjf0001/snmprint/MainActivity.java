84069f52a668405dcb2de8ac4eb027347a698e15
Phillip Ferentinos <pjf0001@auburn.edu>
2015-04-15 19:47:07 -0500

    04-15-2015 7:46pm Working SNMP calls -includes name/IP input

--- app/src/main/java/edu/auburn/eng/csse/comp3710/pjf0001/snmprint/MainActivity.java
+++ app/src/main/java/edu/auburn/eng/csse/comp3710/pjf0001/snmprint/MainActivity.java
@@ -7,13 +7,14 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends Activity
 {
     public static final String READ_COMMUNITY = "public";
     public static final int    mSNMPVersion = 0; // 0 represents SNMP version=1
-    public static final String BASE_IP = "131.204.116.176";
+    public static final String BASE_IP = "131.204.116.";
     public static final String OID_BASE_LEVEL = "1.3.6.1.2.1.43.11.1.1.9.";
     public static final String OID_BLACK = "1.1";
     public static final String OID_YELLOW = "1.2";
@@ -30,7 +31,6 @@ public class MainActivity extends Activity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("DEBUG", "made it to onCreate");
 
         outputText = (TextView) findViewById(R.id.printDetails);
 
@@ -38,7 +38,6 @@ public class MainActivity extends Activity
         submit.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) {
                 new printTask().execute();
-                Log.i("DEBUG", "made it to onCreate submit");
             }
         });
     }
@@ -53,11 +52,14 @@ public class MainActivity extends Activity
     private class printTask extends AsyncTask<String, Void, String>{
         @Override
         protected String doInBackground(String... params) {
-            String returnVal = prntStat.snmpGet(BASE_IP, READ_COMMUNITY, mSNMPVersion, OID_BASE_LEVEL+OID_BLACK);
-            returnVal += prntStat.snmpGet(BASE_IP, READ_COMMUNITY, mSNMPVersion, OID_BASE_LEVEL+OID_CYAN);
-            returnVal += prntStat.snmpGet(BASE_IP, READ_COMMUNITY, mSNMPVersion, OID_BASE_LEVEL+OID_MAGENTA);
-            returnVal += prntStat.snmpGet(BASE_IP, READ_COMMUNITY, mSNMPVersion, OID_BASE_LEVEL+OID_YELLOW);
-            Log.i("DEBUG", "made it to output");
+            EditText ipEnd = (EditText) findViewById(R.id.ipEnd);
+            EditText name = (EditText) findViewById(R.id.printName);
+            String useIP = ipEnd.getText().toString();
+            String  returnVal  = name.getText().toString() + "\n";
+                    returnVal += prntStat.snmpGet(BASE_IP+useIP, READ_COMMUNITY, mSNMPVersion, OID_BASE_LEVEL+OID_BLACK);
+                    returnVal += prntStat.snmpGet(BASE_IP+useIP, READ_COMMUNITY, mSNMPVersion, OID_BASE_LEVEL+OID_CYAN);
+                    returnVal += prntStat.snmpGet(BASE_IP+useIP, READ_COMMUNITY, mSNMPVersion, OID_BASE_LEVEL+OID_MAGENTA);
+                    returnVal += prntStat.snmpGet(BASE_IP+useIP, READ_COMMUNITY, mSNMPVersion, OID_BASE_LEVEL+OID_YELLOW);
             return returnVal;
         }
         protected void onPostExecute(String str)
