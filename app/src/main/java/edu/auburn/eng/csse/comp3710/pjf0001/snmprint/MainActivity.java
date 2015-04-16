e9d77df3c0411093bbb171198a5a016a9e6bc62c
Phillip Ferentinos <pjf0001@auburn.edu>
2015-04-15 22:20:58 -0500

    04-15-2015 10:18 -not working settings page

--- app/src/main/java/edu/auburn/eng/csse/comp3710/pjf0001/snmprint/MainActivity.java
+++ app/src/main/java/edu/auburn/eng/csse/comp3710/pjf0001/snmprint/MainActivity.java
@@ -1,16 +1,21 @@
 package edu.auburn.eng.csse.comp3710.pjf0001.snmprint;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends Activity
+public class MainActivity extends ActionBarActivity
 {
     public static final String READ_COMMUNITY = "public";
     public static final int    mSNMPVersion = 0; // 0 represents SNMP version=1
@@ -20,6 +25,7 @@ public class MainActivity extends Activity
     public static final String OID_YELLOW = "1.2";
     public static final String OID_CYAN = "1.3";
     public static final String OID_MAGENTA = "1.4";
+    private static final int RESULT_SETTINGS = -1;
 
     Button submit;
     TextView outputText;
@@ -31,9 +37,9 @@ public class MainActivity extends Activity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        showUserSettings();
 
         outputText = (TextView) findViewById(R.id.printDetails);
-
         submit = (Button) findViewById(R.id.submitButton);
         submit.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) {
@@ -45,8 +51,50 @@ public class MainActivity extends Activity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.settings, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.menu_settings:
+                Intent i = new Intent(this, UserSettingActivity.class);
+                startActivityForResult(i, RESULT_SETTINGS);
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        showUserSettings();
+
+    }
+
+    private void showUserSettings() {
+        SharedPreferences sharedPrefs = PreferenceManager
+                .getDefaultSharedPreferences(this);
+
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("\n Username: "
+                + sharedPrefs.getString("prefUsername", "NULL"));
+
+        builder.append("\n Send report:"
+                + sharedPrefs.getBoolean("prefSendReport", false));
+
+        builder.append("\n Sync Frequency: "
+                + sharedPrefs.getString("prefSyncFrequency", "NULL"));
+
+        builder.append("\n Mod Number: "
+                + sharedPrefs.getInt("modified", 0));
+
+        TextView settingsTextView = (TextView) findViewById(R.id.textUserSettings);
+
+        settingsTextView.setText(builder.toString());
     }
 
     private class printTask extends AsyncTask<String, Void, String>{
